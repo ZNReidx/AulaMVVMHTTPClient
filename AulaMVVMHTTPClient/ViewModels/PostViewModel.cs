5d34f8523709c6105d294ba5c76350ff23b587c2
Thiago <estrelathiago17@gmail.com>
2024-09-18 14:41:06 -0300

    Add project files.

---
+++ AulaMVVMHTTPClient/ViewModels/PostViewModel.cs
@@ -0,0 +1,33 @@
+﻿using AulaMVVMHTTPClient.Models;
+using AulaMVVMHTTPClient.Services;
+using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace AulaMVVMHTTPClient.ViewModels
+{
+    public partial class PostViewModel : ObservableObject
+    {
+        [ObservableProperty]
+        ObservableCollection<Post> posts;
+        public PostServices PostServices;
+        public ICommand getPostsCommand { get;}
+
+        public PostViewModel(){
+            getPostsCommand = new Command(getPosts);
+        }
+        public async void getPosts()
+        {
+            //Buscar os dados da API
+            PostServices posts = new PostServices();
+            Posts = await PostServices.GetPosts();
+
+
+        }
+    }
+}
